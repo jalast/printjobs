e0b72222af8f87ab96187da73017d21147f286c9
Larry Stamper <james.l.stamper@gmail.com>
2016-06-04 15:07:09 -0700

    Passed Unit Tests

--- src/main/java/printjobs/PrintJobs.java
+++ src/main/java/printjobs/PrintJobs.java
@@ -3,28 +3,28 @@ package printjobs;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Stream;
 
 /**
  * Created by Larry on 6/1/16.
  */
 public class PrintJobs {
 
+    private final Map<JobType, PageRates> rates;
+
     public PrintJobs() {
-        Map<JobType, PageRates> rates = new HashMap<JobType, PageRates>();
+        rates = new HashMap<JobType, PageRates>();
         rates.put(JobType.SINGLE_SIDED, new PageRates(.15, .25));
         rates.put(JobType.DOUBLE_SIDED, new PageRates(.10, .20));
-
-
     }
 
-    double report(InputStream inputStream) {
-//        Collector<Job, ?, ?> collector;
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-//        reader.lines().map(s -> new Job(s.split(", ")));
-        return 0;
+    double report(InputStream inputStream, OutputStream outputStream) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        PrintStream printStream = new PrintStream(outputStream);
+        return reader.lines()
+                .map(s -> new Job(rates, s.split(", ")))
+                .map((job) -> { printStream.println(job); return job;})
+                .mapToDouble(Job::getCharge)
+                .sum();
     }
 
     /**
@@ -33,30 +33,50 @@ public class PrintJobs {
     private static class Job {
         int totalPages;
         int colorPages;
-        boolean doubleSided;
+        JobType jobType;
+        private double charge;
 
-        Job(int totalPages, int colorPages, boolean doubleSided) {
+        Job(Map<JobType, PageRates> rates, int totalPages, int colorPages, boolean doubleSided) {
             this.totalPages = totalPages;
             this.colorPages = colorPages;
-            this.doubleSided = doubleSided;
+            this.jobType = doubleSided ? JobType.DOUBLE_SIDED : JobType.SINGLE_SIDED;
+            charge = reckon(rates);
         }
 
-        Job(String... args) {
-            this(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Boolean.parseBoolean(args[2]));
+        private double reckon(Map<JobType, PageRates> rates) {
+            PageRates r = rates.get(jobType);
+            return r.bwRate * (totalPages - colorPages) + r.getColorRate() * colorPages;
+        }
+
+        Job(Map<JobType, PageRates> rates, String... args) {
+            this(rates, Integer.parseInt(args[0]), Integer.parseInt(args[1]), Boolean.parseBoolean(args[2]));
+        }
+
+        double getCharge() {
+            return charge;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%.2f %s %d Pages %d B&W %d Color",
+                    charge, jobType.abbr(), totalPages, totalPages - colorPages, colorPages);
         }
     }
 
     /**
      * Created by Larry on 6/1/16.
      */
-    public static enum JobType {
-        SINGLE_SIDED, DOUBLE_SIDED
+    private enum JobType {
+        SINGLE_SIDED, DOUBLE_SIDED;
+        public String abbr() {
+            return this == SINGLE_SIDED ? "SS" : "DS";
+        }
     }
 
     /**
      * Created by Larry on 6/1/16.
      */
-    public static class PageRates {
+    private static class PageRates {
         double bwRate;
         double colorRate;
 
@@ -73,4 +93,18 @@ public class PrintJobs {
             this.colorRate = color;
         }
     }
+
+    public static void main(String[] args) {
+        try {
+            if (args.length > 2) throw new Exception("Usage: printjobs [ input-stream | - [ output-stream | -] ]");
+            InputStream in = (args.length > 0 || args[0].equals("-")) ? System.in : new FileInputStream(args[0]);
+            OutputStream out = (args.length > 1 || args[1].equals("-")) ? System.out : new FileOutputStream(args[0]);
+            double totalCharge = new PrintJobs().report(in, out);
+            new PrintStream(out).printf("Total charges: %.2f%n", totalCharge);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
+
 }
--- src/test/java/printjobs/PrintJobsTest.java
+++ src/test/java/printjobs/PrintJobsTest.java
@@ -2,8 +2,15 @@ package printjobs;
 
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+import static java.util.regex.Pattern.COMMENTS;
+import static java.util.regex.Pattern.DOTALL;
 import static org.junit.Assert.*;
 
 /**
@@ -12,8 +19,28 @@ import static org.junit.Assert.*;
 public class PrintJobsTest {
     @Test
     public void testReport() throws Exception {
-        PrintJobs printJobs = new PrintJobs();
-        assertEquals(64.1, printJobs.report(new FileInputStream("printjobs.csv")), .005);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        assertEquals(64.10, new PrintJobs().report(new FileInputStream("printjobs.csv"), outputStream), .005);
+
+        String actual = new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
+        System.out.print(actual);
+        Scanner scanner = new Scanner(actual);
+        checkLine(scanner, 4.75, "SS", 25, 15, 10);
+        checkLine(scanner, 6.80, "DS", 55, 42, 13);
+        checkLine(scanner, 52.40, "DS", 502, 480, 22);
+        checkLine(scanner, .15, "SS", 1, 1, 0);
+    }
+
+    private void checkLine(Scanner scanner, double charge, String type, int total, int bw, int color) {
+        scanner.useDelimiter("[^.\\d]+");
+        assertEquals(charge, scanner.nextDouble(), .005);
+        scanner.useDelimiter("\\W+");
+        assertEquals(type, scanner.next());
+        scanner.useDelimiter("\\D+");
+        assertEquals(total, scanner.nextInt());
+        assertEquals(bw, scanner.nextInt());
+        assertEquals(color, scanner.nextInt());
     }
 
 }
